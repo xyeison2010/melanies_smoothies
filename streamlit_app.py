830bd5af4d1399fbeb4cd906089ebdf8bd35d0f9
Leonardo Huaca <84203012+xyeison2010@users.noreply.github.com>
2025-04-28 19:17:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients_list:
 
         st.subheader(f'{fruit_chosen} Nutrition Information')
         # Llamada a la API
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + str(search_on))
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # Armado del insert
